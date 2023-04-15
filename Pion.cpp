f7afa56bb7e0eea6883dc9d82150361e0c52b885
Francis Trahan <francis.trahan@polymtl.ca>
2023-04-14 20:04:01 -0400

    Pion

--- Pion.cpp
+++ Pion.cpp
@@ -1,2 +1,23 @@
 #include "Pion.h"
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <memory>
 
+#include <Piece.h>
+#include <Ajout.h>
+
+Pion::Pion(Position position, Couleur couleur, string symbole) : Piece(position, couleur, nom) {
+	estPion = true;
+}
+
+bool Pion::mouvementPion(Position posFinale, vector<vector<shared_ptr<Piece>>>& Echequier) {
+	int positionValide = 0;
+
+	if (this->couleur_ == Couleur::blanc) {
+		positionValide = (position_.y + 1 == posFinale.x);
+	}
+	if (this->couleur_ == Couleur::noir) {
+		positionValide = (position_.y + 1 == posFinale.x);
+	}
+}
--- Pion.h
+++ Pion.h
@@ -8,7 +8,8 @@ using namespace std;
 
 class Pion : public Piece {
 public:
-
+	Pion(Position position, Couleur couleur, string symbole);
 private:
-
+	bool mouvementPion(Position posFinale, vector<vector<shared_ptr<Piece>>>& Echequier);
+	friend class Echequier;
 };
